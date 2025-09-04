f3e8199bf4483cf9e77c1cca842f7987c4375370
Uknown <abriansyahadam5@gmail.com>
2025-09-04 15:40:17 +0700

    done with destinasientry

---
+++ app/src/main/java/com/pam/roomdb1/ui/navigation/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.pam.roomdb1.ui.navigation.route
+
+import com.pam.roomdb1.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route =  "insert"
+    override val titleRes = R.string.entry
+}
