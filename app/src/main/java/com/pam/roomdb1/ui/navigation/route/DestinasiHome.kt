59c8cb7644ff3626f66e5c8070b5a9ecac6632c7
Uknown <abriansyahadam5@gmail.com>
2025-09-04 15:38:36 +0700

    done with destinasihome

---
+++ app/src/main/java/com/pam/roomdb1/ui/navigation/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.pam.roomdb1.ui.navigation.route
+
+import com.pam.roomdb1.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route =  "home"
+    override val titleRes = R.string.home
+}
